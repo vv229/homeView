f900bd8addd09bd757c5f1746208ce1715a67299
jeremy <show229@outlook.com>
2024-12-28 17:01:13 +0800

    11111111

--- components/landing/Navbar.vue
+++ components/landing/Navbar.vue
@@ -26,8 +26,8 @@ const open = ref(false);
     <header class="flex flex-col lg:flex-row justify-between items-center my-5">
       <div class="flex w-full lg:w-auto items-center justify-between">
         <a href="/" class="text-lg"
-          ><span class="font-bold text-slate-800">Nuxt</span
-          ><span class="text-slate-500">ship</span>
+          ><span class="font-bold text-slate-800">志行</span
+          ><span class="text-slate-500">工作室</span>
         </a>
         <div class="block lg:hidden">
           <button @click="open = !open" class="text-gray-800">
