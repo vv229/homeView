2c7a935839d68730ba3073ca4ab9e44ed48e5352
jeremy <show229@outlook.com>
2024-12-29 16:21:53 +0800

    x

--- components/landing/Sectionhead.vue
+++ components/landing/Sectionhead.vue
@@ -9,7 +9,7 @@ defineProps({
 <template>
   <div :class="['mt-16', align === 'center' && 'text-center']">
     <h1 class="text-4xl lg:text-5xl font-bold lg:tracking-tight">
-      <slot name="title">Title</slot>
+      <slot name="title">cccc</slot>
     </h1>
     <p class="text-lg mt-4 text-slate-600">
       <slot name="desc">Some description goes here</slot>
